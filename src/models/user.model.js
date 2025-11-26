e6da418252ebce88efcd1badfb15e1734cbd7a29
Saumya Jha <jhasaumya554@gmail.com>
2025-11-26 18:09:18 +0530

    jwt tokens generate method

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -64,4 +64,30 @@ userSchema.methods.isPasswordCorrect = async function (password) {
   // return match;
 };
 
+userSchema.methods.generateAccessToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+      userName: this.userName,
+      email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+      expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+    },
+  );
+};
+
+userSchema.methods.generateRefreshToken = function () {
+  return jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.REFRESH_TOKEN_SECRET,
+    {
+      expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+    },
+  );
+};
+
 export const User = mongoose.model("User", userSchema);
