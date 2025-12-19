663431d5ce182dabcf80af8e6046a5071d305872
Saumya Jha <jhasaumya554@gmail.com>
2025-12-19 08:55:25 +0530

    paginated getVideoComments method

--- src/controllers/comment.controller.js
+++ src/controllers/comment.controller.js
@@ -8,6 +8,51 @@ import { Comment } from "../models/comment.model.js";
 const getVideoComments = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   const { page = 1, limit = 10 } = req.query;
+
+  if (!videoId || !isValidObjectId(videoId))
+    throw new ApiError(400, "Invalid videoId!");
+
+  const pipeline = [
+    {
+      $match: {
+        video: new mongoose.Types.ObjectId(videoId),
+      },
+    },
+    {
+      $lookup: {
+        from: "users",
+        localField: "owner",
+        foreignField: "_id",
+        as: "owner",
+        pipeline: [
+          {
+            $project: {
+              _id: 0,
+              userName: 1,
+              fullName: 1,
+              avatar: 1,
+            },
+          },
+        ],
+      },
+    },
+    {
+      $unwind: "$owner",
+    },
+  ];
+
+  if (!pipeline.length) throw new ApiError(404, "Comment not found!");
+
+  const result = await Comment.aggregatePaginate(Comment.aggregate(pipeline), {
+    page,
+    limit,
+  });
+
+  if (!result) throw new ApiError(404, "Error while paginating!");
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, result, "Fetched video comments successfully!"));
 });
 
 const addComment = asyncHandler(async (req, res) => {
