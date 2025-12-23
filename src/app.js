18feb18c705c02fe4e54223e6d76aa5af6f72c81
Saumya Jha <jhasaumya554@gmail.com>
2025-12-23 18:50:52 +0530

    started dashboard functionality

--- src/app.js
+++ src/app.js
@@ -23,6 +23,7 @@ import tweetRouter from "./routes/tweet.routes.js";
 import subscriptionRouter from "./routes/subscription.routes.js";
 import commentRouter from "./routes/comment.routes.js";
 import likeRouter from "./routes/like.routes.js";
+import dashboardRouter from "./routes/dashboard.routes.js";
 
 // handling routes
 app.use("/api/v1/healthCheck", healthCheckRouter);
@@ -30,8 +31,9 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/tweets", tweetRouter);
 app.use("/api/v1/subscriptions", subscriptionRouter);
 
+app.use("/api/v1/comments", commentRouter);
 app.use("/api/v1/likes", likeRouter);
 
-app.use("/api/v1/comments", commentRouter);
+app.use("/api/v1/dashboard", dashboardRouter);
 
 export { app };
---
+++ src/controllers/dashboard.controller.js
@@ -0,0 +1,26 @@
+import { Video } from "../models/video.model.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { asyncHandler } from "../utils/asyncHandler.js";
+
+const getChannelStats = asyncHandler(async (req, res) => {});
+
+const getChannelVideos = asyncHandler(async (req, res) => {
+  const channelVideos = await Video.find({
+    owner: req.user._id,
+  });
+
+  if (!channelVideos) throw new ApiError(404, "Videos not found!");
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        channelVideos,
+        "Channel Videos fetched successfully!",
+      ),
+    );
+});
+
+export { getChannelStats, getChannelVideos };
