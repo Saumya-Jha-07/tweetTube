563cc4aefff0c0c40432e37a9c8af6ef410507ca
Saumya Jha <jhasaumya554@gmail.com>
2025-12-24 23:18:14 +0530

    added dashboard controller

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -1,9 +1,15 @@
+import mongoose from "mongoose";
 import { Video } from "../models/video.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 
-const getChannelStats = asyncHandler(async (req, res) => {});
+const getChannelStats = asyncHandler(async (req, res) => {
+  if (!mongoose.isValidObjectId(req.user._id))
+    throw new ApiError(400, "Invalid owner Id!");
+
+  const totalVideos = await Video.countDocuments({ owner: req.user._id });
+});
 
 const getChannelVideos = asyncHandler(async (req, res) => {
   const channelVideos = await Video.find({
